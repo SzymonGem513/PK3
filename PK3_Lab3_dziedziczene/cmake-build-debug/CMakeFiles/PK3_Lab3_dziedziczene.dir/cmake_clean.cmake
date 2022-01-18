8702a0410979aa99ffdbe9d8de6410fec9f02ec5
Szymon <szymgem00@gmail.com>
2022-01-18 17:34:28 +0100

    Lab3 init

---
+++ PK3_Lab3_dziedziczene/cmake-build-debug/CMakeFiles/PK3_Lab3_dziedziczene.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PK3_Lab3_dziedziczene.dir/main.cpp.obj"
+  "PK3_Lab3_dziedziczene.exe"
+  "PK3_Lab3_dziedziczene.exe.manifest"
+  "PK3_Lab3_dziedziczene.pdb"
+  "libPK3_Lab3_dziedziczene.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PK3_Lab3_dziedziczene.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PK3_Lab3_dziedziczene/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "head.h"
+
+using namespace std;
+
+int main() {
+    cout << "Hello, Wokrld!" << endl;
+    return 0;
+}
